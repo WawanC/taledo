9053110f51f9abeebbadbdd35e745c4765273dfa
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-05-29 03:01:35 +0700

    update todo api route

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -30,6 +30,31 @@ export const createTodo: RequestHandler = async (req, res, next) => {
   }
 };
 
+export const updateTodo: RequestHandler = async (req, res, next) => {
+  try {
+    const todo = await todoModel.findById(req.params.id);
+
+    if (!todo) {
+      return res.status(404).json({
+        type: "NOT_FOUND",
+        message: "Todo not found"
+      });
+    }
+    await todo.updateOne({
+      $set: { title: req.body.title, isCompleted: req.body.isCompleted }
+    });
+
+    const updatedTodo = await todoModel.findById(req.params.id);
+
+    return res.status(200).json({
+      message: "Update todo success",
+      todo: updatedTodo
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const deleteTodo: RequestHandler = async (req, res, next) => {
   try {
     const todo = await todoModel.findById(req.params.id);
--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -5,6 +5,7 @@ const todoRouter = Router();
 
 todoRouter.get("/todos", todoController.getTodos);
 todoRouter.post("/todo", todoController.createTodo);
+todoRouter.put("/todo/:id", todoController.updateTodo);
 todoRouter.delete("/todo/:id", todoController.deleteTodo);
 
 export default todoRouter;
