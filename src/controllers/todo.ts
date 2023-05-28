06fbec780215d8a8f2eca2bf31b37ecd95e81173
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-05-28 22:45:16 +0700

    create todo api route

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -5,6 +5,19 @@ export const getTodos: RequestHandler = async (req, res) => {
   const todos = await todoModel.find();
 
   return res.status(200).json({
+    message: "Fetch todos success",
     todos: todos
+  });
+};
+
+export const createTodo: RequestHandler = async (req, res) => {
+  const newTodo = await todoModel.create({
+    title: req.body.title,
+    isCompleted: false
+  });
+
+  return res.status(200).json({
+    message: "Create todo success",
+    todo: newTodo
   });
 };
--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -4,5 +4,6 @@ import * as todoController from "../controllers/todo";
 const todoRouter = Router();
 
 todoRouter.get("/todos", todoController.getTodos);
+todoRouter.post("/todo", todoController.createTodo);
 
 export default todoRouter;
