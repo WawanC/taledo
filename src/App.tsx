833e82a71e549561ea2b13ee28cc3c50dc03221f
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-05-29 08:22:23 +0700

    add env

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <main>
       <h1 className="text-4xl">Taledo Client</h1>
+      <h2 className="text-2xl">App Name : {import.meta.env.VITE_APP_TITLE}</h2>
     </main>
   );
 };
