b16e8c26ce95ad3a2b24525c98af728155fb7a05
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-06-01 20:05:14 +0700

    update auth serialization

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,6 +4,7 @@ declare global {
   namespace Express {
     export interface User {
       id: string;
+      username: string;
     }
   }
 }
--- src/passport/local-strategy.ts
+++ src/passport/local-strategy.ts
@@ -18,17 +18,24 @@ const verify: VerifyFunction = async (username, password, done) => {
     return done(null, false, { message: "Wrong password" });
   }
 
-  return done(null, { id: user.id });
+  return done(null, { id: user.id, username: user.username });
 };
 
 const localStrategy = new LocalStrategy(verify);
 
 const initializePassportLocal = () => {
   passport.serializeUser((user, done) => {
-    done(null, user);
+    done(null, user.id);
   });
 
-  passport.deserializeUser((user: Express.User, done) => {
+  passport.deserializeUser(async (id: string, done) => {
+    const user = await prisma.user.findUnique({
+      where: { id: id },
+      select: { id: true, username: true }
+    });
+    if (!user) {
+      return done(null, null);
+    }
     done(null, user);
   });
 
