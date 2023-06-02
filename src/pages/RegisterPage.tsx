093fdfe115fe8a9ab61cae7497d3f6b0e65a5376
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-06-02 14:00:48 +0700

    add loading when user register

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -66,61 +66,69 @@ const RegisterPage: React.FC = () => {
   return (
     <main className="flex flex-col items-center px-4 py-8 gap-8">
       <h1 className="text-4xl font-bold">Create New Account</h1>
-      <Formik
-        initialValues={initialRegisterFormValues}
-        onSubmit={registerFormHandler}
-      >
-        {({ values }) => (
-          <Form className="text-xl w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
-            {(values.error || registerUser.isError) && (
-              <div className="flex justify-center text-lg">
-                <span className="text-red-500 text-center">
-                  {values.error || getServerErrorMessage(registerUser.error)}
-                </span>
-              </div>
-            )}
+      {registerUser.isLoading ? (
+        <p className="text-xl">Loading...</p>
+      ) : (
+        <Formik
+          initialValues={initialRegisterFormValues}
+          onSubmit={registerFormHandler}
+        >
+          {({ values, isSubmitting }) => (
+            <Form className="text-xl w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+              {(values.error || registerUser.isError) && (
+                <div className="flex justify-center text-lg">
+                  <span className="text-red-500 text-center">
+                    {values.error || getServerErrorMessage(registerUser.error)}
+                  </span>
+                </div>
+              )}
 
-            <div className="flex flex-col">
-              <label htmlFor="username">Username :</label>
-              <Field
-                id="username"
-                type="text"
-                className="border-b border-black outline-none py-2"
-                placeholder="Enter your username"
-                required
-                name="username"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="password">Password :</label>
-              <Field
-                id="password"
-                type="password"
-                className="border-b border-black outline-none py-2"
-                placeholder="Enter your password"
-                required
-                name="password"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="password2">Repeat Password :</label>
-              <Field
-                id="password2"
-                type="password"
-                className="border-b border-black outline-none py-2"
-                placeholder="Repeat your password"
-                required
-                name="password2"
-              />
-            </div>
-            <div className="flex justify-center">
-              <button type="submit" className="bg-primary p-2">
-                Register
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+              <div className="flex flex-col">
+                <label htmlFor="username">Username :</label>
+                <Field
+                  id="username"
+                  type="text"
+                  className="border-b border-black outline-none py-2"
+                  placeholder="Enter your username"
+                  required
+                  name="username"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="password">Password :</label>
+                <Field
+                  id="password"
+                  type="password"
+                  className="border-b border-black outline-none py-2"
+                  placeholder="Enter your password"
+                  required
+                  name="password"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="password2">Repeat Password :</label>
+                <Field
+                  id="password2"
+                  type="password"
+                  className="border-b border-black outline-none py-2"
+                  placeholder="Repeat your password"
+                  required
+                  name="password2"
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="bg-primary p-2"
+                  disabled={isSubmitting}
+                >
+                  Register
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      )}
     </main>
   );
 };
