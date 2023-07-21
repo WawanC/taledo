52cc2f1f538670d221e73aafb693384c130cb05c
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-07-21 09:16:31 +0700

    basic dnd board feature

--- client/src/pages/BoardPage.tsx
+++ client/src/pages/BoardPage.tsx
@@ -1,9 +1,34 @@
-import { DndContext } from "@dnd-kit/core";
+import {
+  DndContext,
+  DragEndEvent,
+  useDraggable,
+  useDroppable
+} from "@dnd-kit/core";
 import { FC, useState } from "react";
+// import { CSS } from "@dnd-kit/utilities";
 
-const BoardItem: FC<{ title: string }> = (props) => {
+const BoardItem: FC<{ title: string; section: string }> = (props) => {
+  const { setNodeRef, attributes, listeners, transform } = useDraggable({
+    id: props.title,
+    data: { section: props.section }
+  });
+  // const style = { transform: CSS.Transform.toString(transform) };
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      }
+    : undefined;
+
   return (
-    <li className="p-4 bg-black rounded text-center shadow">{props.title}</li>
+    <li
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="p-4 bg-black rounded text-center shadow"
+    >
+      {props.title}
+    </li>
   );
 };
 
@@ -11,13 +36,19 @@ const BoardSection: FC<{
   title: string;
   items: string[];
 }> = (props) => {
+  const { setNodeRef } = useDroppable({
+    id: props.title
+  });
   return (
-    <section className="flex flex-col gap-4 bg-bold w-1/3 p-4 rounded">
+    <section
+      ref={setNodeRef}
+      className="flex flex-col gap-4 bg-bold w-1/3 p-4 rounded"
+    >
       <h1 className="text-2xl font-bold text-center">{props.title}</h1>
       <hr />
       <ul className="flex flex-col gap-4">
         {props.items.map((item) => (
-          <BoardItem key={item} title={item} />
+          <BoardItem key={item} title={item} section={props.title} />
         ))}
       </ul>
     </section>
@@ -25,12 +56,31 @@ const BoardSection: FC<{
 };
 
 const BoardPage: FC = () => {
-  const [items] = useState({
+  const [items, setItems] = useState({
     Plan: ["Learn React JS", "Learn Node JS"],
     Done: ["Learn Javascript"]
   });
+
+  const dragEndHandler = (e: DragEndEvent) => {
+    // console.log("active:", e.active.data.current);
+    // console.log("over:", e.over?.id);
+
+    const selectedItem = e.active.id as string;
+    const initialSection = e.active.data.current?.section as keyof typeof items;
+    const targetSection = e.over?.id as keyof typeof items;
+
+    if (initialSection !== targetSection) {
+      const newItems = { ...items };
+      newItems[initialSection] = items[initialSection].filter(
+        (item) => item !== selectedItem
+      );
+      newItems[targetSection].push(selectedItem);
+      setItems(newItems);
+    }
+  };
+
   return (
-    <DndContext>
+    <DndContext onDragEnd={dragEndHandler}>
       <main
         className="dark:bg-semi_bold flex-1 
         flex justify-center gap-4 
